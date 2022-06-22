d8da1316230a34678ce57dfe5a3e32624aa6a406
redanthrax <redanthraxx@gmail.com>
2022-06-22 13:03:45 -0700

    added unix support/tested builds

--- agent/agent_unix.go
+++ agent/agent_unix.go
@@ -1,13 +1,4 @@
-/*
-Copyright 2022 AmidaWare LLC.
-
-Licensed under the Tactical RMM License Version 1.0 (the “License”).
-You may only use the Licensed Software in accordance with the License.
-A copy of the License is available at:
-
-https://license.tacticalrmm.com
-
-*/
+// +build !windows
 
 package agent
 
