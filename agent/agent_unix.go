d3753535d53a8cd4050f3eacdc53b314f116a70b
wh1te909 <dcparsi@gmail.com>
2022-06-22 20:20:19 +0000

    add back license

--- agent/agent_unix.go
+++ agent/agent_unix.go
@@ -1,4 +1,16 @@
+//go:build !windows
 // +build !windows
+
+/*
+Copyright 2022 AmidaWare LLC.
+
+Licensed under the Tactical RMM License Version 1.0 (the “License”).
+You may only use the Licensed Software in accordance with the License.
+A copy of the License is available at:
+
+https://license.tacticalrmm.com
+
+*/
 
 package agent
 
