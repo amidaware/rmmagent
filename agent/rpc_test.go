5917d0337450c33558ad9df7162e93f20e1c031a
redanthrax <redanthraxx@gmail.com>
2022-06-24 09:29:00 -0700

    run rpc comment

--- agent/rpc_test.go
+++ agent/rpc_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 )
 
+//uncomment to test rpc, comment to add back before commit, this test will always timeout
 func TestRunRPC(t *testing.T) {
 	a := New(lg, version)
 	a.RunRPC()
