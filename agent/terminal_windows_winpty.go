b90bc9719eeee4dc12faaa8503e6d2733fb34b42
wh1te909 <7434746+wh1te909@users.noreply.github.com>
2026-04-24 14:15:43 -0700

    add header

--- agent/terminal_windows_winpty.go
+++ agent/terminal_windows_winpty.go
@@ -1,5 +1,16 @@
 //go:build windows
 
+/*
+Copyright 2025 AmidaWare Inc.
+
+Licensed under the Tactical RMM License Version 1.0 (the “License”).
+You may only use the Licensed Software in accordance with the License.
+A copy of the License is available at:
+
+https://license.tacticalrmm.com
+
+*/
+
 package agent
 
 import (
